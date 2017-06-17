f47e0d6f6a28a6130f1912cab5b7bdc4e7c8a800
coreyarch1234 <harrilal@mit.edu>
2017-06-17 14:59:51 -0700

    found structured way to clean nurse data. created json

--- ingestion/pipeline.py
+++ ingestion/pipeline.py
@@ -1,4 +1,5 @@
 from os.path import join, dirname
+import csv
 import pandas as pd
 import numpy as np
 from pymongo import MongoClient
@@ -17,12 +18,33 @@ and store data in mongo. The fields we're interested in storing are:
 
 Check server/models/Record.js for an example of the schema.
 
+Subtasks:
+
+1) Use csv.DictReader to create JSON where keys are the questions and values are the answers (i.e. - {salary: 28})
+2) Exclude keys we don't need. All we need is: experience, education, salary, department, location, patientNurseRatio.
+Note that the keys will be the full questions at first, so we need to convert that to the Record.js model attribute keys
+3) Populate the Record model with the salary and patientNurseRatio fields
+
 """
+def create_nurse_json(csv):
+    nurse_info_list = [] #This will be my nurse JSON. Is a list of dictionaries. The keys are the questions (i.e. - salary?) and the values are
+    # are dictionaries where the keys are incremental indices (0,1,..) and the values are the answers to the question for each nurse.
+    index = 0
+    nurse_questions, nurse_answers = {}, {}
+    for line in csv:
+        nurse_answers[index] = line.iloc[0,0]
+        index += 1
+    nurse_questions["What (City, State) are you located in?"] = nurse_answers
+    nurse_info_list.append(nurse_questions)
+    return nurse_info_list
 
 def main():
     client = MongoClient('mongodb://localhost:27017/')
     db = client['clipboardinterview']
-    df = pd.read_csv(join(dirname(__file__), '../data/projectnurse.csv'))
+    df = pd.read_csv(join(dirname(__file__), '../data/projectnurse.csv'), chunksize=1, header=None, encoding='utf-8')
+    print create_nurse_json(df)
+
+
 
 if __name__ == "__main__":
     main()

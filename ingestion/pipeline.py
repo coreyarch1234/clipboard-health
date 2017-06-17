62dcdd4fa9a81565a1c85cf55dbf6d9a134a7882
coreyarch1234 <harrilal@mit.edu>
2017-06-17 15:40:54 -0700

    created json for each question and for all nurses

--- ingestion/pipeline.py
+++ ingestion/pipeline.py
@@ -26,24 +26,25 @@ Note that the keys will be the full questions at first, so we need to convert th
 3) Populate the Record model with the salary and patientNurseRatio fields
 
 """
-def create_nurse_json(csv):
-    nurse_info_list = [] #This will be my nurse JSON. Is a list of dictionaries. The keys are the questions (i.e. - salary?) and the values are
-    # are dictionaries where the keys are incremental indices (0,1,..) and the values are the answers to the question for each nurse.
+def create_nurse_json(csv, column, question_key):
     index = 0
-    nurse_questions, nurse_answers = {}, {}
+    nurse_questions_dict, nurse_answers_dict = {}, {}
     for line in csv:
-        nurse_answers[index] = line.iloc[0,0]
+        nurse_answers_dict[index] = line.iloc[0,column]
         index += 1
-    nurse_questions["What (City, State) are you located in?"] = nurse_answers
-    nurse_info_list.append(nurse_questions)
-    return nurse_info_list
+    nurse_questions_dict[question_key] = nurse_answers_dict
+    return nurse_questions_dict
 
 def main():
     client = MongoClient('mongodb://localhost:27017/')
     db = client['clipboardinterview']
-    df = pd.read_csv(join(dirname(__file__), '../data/projectnurse.csv'), chunksize=1, header=None, encoding='utf-8')
-    print create_nurse_json(df)
-
+    list_of_questions = ["What (City, State) are you located in?", "What's your highest level of education?", "Department", "How's the employee turnover?", "How many years of experience do you have?", "What is/was your length of orientation/training?", "What is the Nurse - Patient Ratio?", "What is your hourly rate ($/hr)?", "What's Your Shift Length?", "Which Shift?", "Other", "Full-Time/Part-Time?", "Do you have any special skills that set you apart from other nurses? (examples: CCRN, CNOR, Special Procedures, etc.)", "Would you recommend your department to another nurse?", "How did you hear about Project Nurse?", "Start Date (UTC)", "Submit Date (UTC)"]
+    nurse_info_list = [] #This will be my nurse JSON. Is a list of dictionaries. The keys are the questions (i.e. - salary?) and the values are
+    # are dictionaries where the keys are incremental indices (0,1,..) and the values are the answers to the question for each nurse.
+    for column in range(len(list_of_questions)):
+        df = pd.read_csv(join(dirname(__file__), '../data/projectnurse.csv'), chunksize=1)
+        nurse_info_list.append(create_nurse_json(df, column, list_of_questions[column]))
+    print nurse_info_list
 
 
 if __name__ == "__main__":
